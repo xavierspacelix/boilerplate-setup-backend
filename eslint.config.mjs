a4925ecbad244e470c2cb53a988dfdc43cfb4c05
xavierspacelix <juanakbar361@gmail.com>
2025-12-17 13:50:16 +0700

    chore: add husky hooks and lint-staged configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,41 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+  {
+    ignores: ['**/node_modules/**', '**/dist/**', '**/.git/**'],
+    files: ['**/*.{js,mjs,ts,mts,cts}'],
+    plugins: {
+      js,
+      prettier: prettierPlugin,
+    },
+    extends: ['js/recommended', prettierConfig],
+    languageOptions: { globals: globals.browser },
+    rules: {
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+      '@typescript-eslint/no-empty-object-type': [
+        'error',
+        {
+          allowObjectTypes: 'always',
+        },
+      ],
+    },
+  },
+  tseslint.configs.recommended,
+]);
