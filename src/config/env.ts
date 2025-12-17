c7eb561429c9237f0a1c0a65ee7677db3efc1dca
xavierspacelix <juanakbar361@gmail.com>
2025-12-17 16:33:22 +0700

    feat(config): add new environment variables for file storage and rate limiting

--- src/config/env.ts
+++ src/config/env.ts
@@ -15,16 +15,34 @@ const envSchema = z.object({
   REDIS_PORT: z.coerce.number().int().positive().default(6379),
   REDIS_USER: z.string().min(1).default('default'),
   REDIS_PASSWORD: z.string().min(1).default(''),
+  FILE_HOST: z.string().default('localhost:3001/api'),
+  MINIO_MODE: z.string().default('http'),
+  MINIO_INTERNAL_ENDPOINT: z.string().default('192.168.1.1'),
+  MINIO_PUBLIC_ENDPOINT: z.string().default('192.168.1.1'),
+  MINIO_ROOT_USER: z.string().default('your_minio_user'),
+  MINIO_ROOT_PASSWORD: z.string('your_minio_password'),
+  MINIO_PORT: z.coerce.number().default(9000),
+  MINIO_BUCKETNAME: z.string().default('kbgroupware'),
+  COMMON_RATE_LIMIT_WINDOW_MS: z.coerce.number().int().positive().default(1000),
   COMMON_RATE_LIMIT_MAX_REQUESTS: z.coerce
     .number()
     .int()
     .positive()
     .default(1000),
-  COMMON_RATE_LIMIT_WINDOW_MS: z.coerce.number().int().positive().default(1000),
   CORS_ORIGIN: z
     .string()
     .default('http://localhost:3001')
     .transform((str) => str.split(',').map((url) => url.trim())),
+  ALLOWED_MIME_TYPES: z
+    .string()
+    .transform((str) => str.split(',').map((s) => s.trim()))
+    .default([
+      'image/png',
+      'image/jpeg',
+      'application/pdf',
+      'application/zip',
+      'application/x-rar-compressed',
+    ]),
 });
 
 const parsedEnv = envSchema.safeParse(process.env);
